d29e51a0e6efe43580444478ac8558713661cf57
nordluma <maxnordlund.95@hotmail.com>
2023-06-21 23:28:01 +0300

    feat: rendering objects

--- part_1/part1/src/App.js
+++ part_1/part1/src/App.js
@@ -1,27 +1,12 @@
-const Hello = (props) => {
-    console.log(props);
-    return (
-        <div>
-            <p>Hello {props.name}, you are {props.age} years old</p>
-        </div>
-    );
-}
-
-const Footer = () => {
-    return (
-        <div>
-            greeting app created by
-            <a href="https://github.com">nordluma</a>
-        </div>
-    );
-}
-
 const App = () => {
+    const friends = [
+        { name: "Leevi", age: 4 },
+        { name: "Venla", age: 10 },
+    ];
     return (
         <>
-            <h1>Greetings</h1>
-            <Hello name="Maya" age={26 + 10} />
-            <Footer />
+            <p>{friends[0].name} {friends[0].age}</p>
+            <p>{friends[1].name} {friends[1].age}</p>
         </>
     );
 }
