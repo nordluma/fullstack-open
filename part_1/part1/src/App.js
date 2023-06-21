db699eff5b51ce412e071d1adad885c85910f535
nordluma <maxnordlund.95@hotmail.com>
2023-06-21 23:16:46 +0300

    feat: moar props

--- part_1/part1/src/App.js
+++ part_1/part1/src/App.js
@@ -1,18 +1,28 @@
 const Hello = (props) => {
+    console.log(props);
     return (
         <div>
-            <p>Hello {props.name}</p>
+            <p>Hello {props.name}, you are {props.age} years old</p>
+        </div>
+    );
+}
+
+const Footer = () => {
+    return (
+        <div>
+            greeting app created by
+            <a href="https://github.com">nordluma</a>
         </div>
     );
 }
 
 const App = () => {
     return (
-        <div>
+        <>
             <h1>Greetings</h1>
-            <Hello name="Maya" />
-            <Hello name="Pekka" />
-        </div>
+            <Hello name="Maya" age={26 + 10} />
+            <Footer />
+        </>
     );
 }
 
