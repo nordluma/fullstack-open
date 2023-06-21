6149ef4f88371c738c20dd45166a7658edd8d470
nordluma <maxnordlund.95@hotmail.com>
2023-06-22 00:11:41 +0300

    refactor: change Content to take all course information
    Content takes all course information and then passes the values to Part
    component

--- part_1/tasks/kurssitiedot/src/App.js
+++ part_1/tasks/kurssitiedot/src/App.js
@@ -10,6 +10,19 @@ const Part = (props) => {
   );
 };
 
+const Content = (props) => {
+  const { part1, part2, part3 } = props.parts;
+  const { exercises1, exercises2, exercises3 } = props.exercises;
+
+  return (
+    <div>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+    </div>
+  );
+};
+
 const Total = (props) => {
   return <p>Number of exercises {props.totalExercises}</p>;
 };
@@ -26,9 +39,10 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
+      <Content
+        parts={{ part1, part2, part3 }}
+        exercises={{ exercises1, exercises2, exercises3 }}
+      />
       <Total totalExercises={exercises1 + exercises2 + exercises3} />
     </div>
   );
