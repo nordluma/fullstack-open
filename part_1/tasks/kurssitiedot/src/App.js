edbd0d428a00a117326119718e6ffd2917c35613
nordluma <maxnordlund.95@hotmail.com>
2023-06-21 23:51:49 +0300

    refactor: move content to own component

--- part_1/tasks/kurssitiedot/src/App.js
+++ part_1/tasks/kurssitiedot/src/App.js
@@ -2,6 +2,14 @@ const Header = (props) => {
   return <h1>{props.course}</h1>;
 };
 
+const Content = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+};
+
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals in React";
@@ -14,15 +22,9 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   );
