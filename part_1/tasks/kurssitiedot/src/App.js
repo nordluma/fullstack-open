580a6298733893e65bb6752bfa4bdef72dfbd51c
nordluma <maxnordlund.95@hotmail.com>
2023-06-21 23:58:34 +0300

    fix: rename Content to Part

--- part_1/tasks/kurssitiedot/src/App.js
+++ part_1/tasks/kurssitiedot/src/App.js
@@ -2,7 +2,7 @@ const Header = (props) => {
   return <h1>{props.course}</h1>;
 };
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <p>
       {props.part} {props.exercises}
@@ -26,9 +26,9 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
       <Total totalExercises={exercises1 + exercises2 + exercises3} />
     </div>
   );
