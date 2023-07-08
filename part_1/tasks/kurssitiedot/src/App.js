f2f9abc01a46f3f11262ba65aa6e504469f57639
nordluma <maxnordlund.95@hotmail.com>
2023-07-09 01:04:22 +0300

    refactor: change course parts to objects
    Changed the part information to objects which are passed to Content as
    props

--- part_1/tasks/kurssitiedot/src/App.js
+++ part_1/tasks/kurssitiedot/src/App.js
@@ -5,20 +5,18 @@ const Header = (props) => {
 const Part = (props) => {
   return (
     <p>
-      {props.part} {props.exercises}
+      {props.name} {props.exercises}
     </p>
   );
 };
 
 const Content = (props) => {
   const { part1, part2, part3 } = props.parts;
-  const { exercises1, exercises2, exercises3 } = props.exercises;
-
   return (
     <div>
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
+      <Part name={part1.name} exercises={part1.exercises} />
+      <Part name={part2.name} exercises={part2.exercises} />
+      <Part name={part3.name} exercises={part3.exercises} />
     </div>
   );
 };
@@ -29,21 +27,26 @@ const Total = (props) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals in React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        parts={{ part1, part2, part3 }}
-        exercises={{ exercises1, exercises2, exercises3 }}
+      <Content parts={{ part1, part2, part3 }} />
+      <Total
+        totalExercises={part1.exercises + part2.exercises + part3.exercises}
       />
-      <Total totalExercises={exercises1 + exercises2 + exercises3} />
     </div>
   );
 };
